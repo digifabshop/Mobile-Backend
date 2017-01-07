98b882da229e4f8c317aa130a647d0b0df1c439a
Jack Turner <jack@turner.io>
2017-01-07 18:08:11 -0500

    Menu styling in its initial form

--- html_sketches/home_with_menu/home_with_menu.js
+++ html_sketches/home_with_menu/home_with_menu.js
@@ -1,18 +1,27 @@
 $(function () {
 
-	var $bod = $('body')
+	var $bod = $('body'),
+			$menu_button = $('#menu-button'),
+			$menu_close_button = $('#menu-close-button')
 
 	$bod.on('click', function() {
-		$bod.removeClass('hide-info')
-		setTimeout( function() {
-			$bod.addClass('hide-info')
-		}, 3000)		
+		$bod.toggleClass('hide-hud')
 	})
 
 	$(window).on('load', function() {
 		setTimeout( function() {
-			$bod.addClass('hide-info')
+			$bod.addClass('hide-hud')
 		}, 2000)
 	})
+
+	$menu_button.on( 'click', function(e) {
+	  $bod.addClass('show-menu')
+	  e.preventDefault()
+	} )
+
+	$menu_close_button.on( 'click', function(e) {
+	  $bod.removeClass('show-menu')
+	  e.preventDefault()
+	} )
 
 })
