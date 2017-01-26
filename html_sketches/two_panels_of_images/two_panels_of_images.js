4d71201a239eaeeda3b77a17ee111eaec427078f
Jack Turner <jack@turner.io>
2017-01-26 12:58:57 -0500

    Got a two panel scrolling+flickity thing

---
+++ html_sketches/two_panels_of_images/two_panels_of_images.js
@@ -0,0 +1,29 @@
+$(function () {
+
+	var $bod = $('body'),
+			$menu_button = $('#menu-button'),
+			$menu_close_button = $('#menu-close-button')
+
+	$bod.on('click', function() {
+		$bod.toggleClass('hide-hud')
+	})
+
+	$(window).on('load', function() {
+		setTimeout( function() {
+			$bod.addClass('hide-hud')
+		}, 2000)
+	})
+
+	$menu_button.on( 'click', function(e) {
+	  $bod.addClass('show-menu')
+	  e.preventDefault()
+	} )
+
+	$menu_close_button.on( 'click', function(e) {
+	  $bod.removeClass('show-menu')
+	  e.preventDefault()
+	} )
+
+	$('#completed-images').flickity()
+
+})
