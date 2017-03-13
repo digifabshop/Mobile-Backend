d8d53841451fbe0a390632baaf38377818ec9c66
Jeff Loiselle <jeff@loiselles.com>
2017-03-13 10:27:45 -0700

    made separate dirs for ui and admin

--- admin/src/app/main/main.controller.js
+++ admin/src/app/main/main.controller.js
@@ -27,16 +27,3 @@
 
   }
 })();
-
-
-$(".carousel").swipe({
-
-  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-    if (direction == 'left') $(this).carousel('next');
-    if (direction == 'right') $(this).carousel('prev');
-
-  },
-  allowPageScroll:"vertical"
-
-});
