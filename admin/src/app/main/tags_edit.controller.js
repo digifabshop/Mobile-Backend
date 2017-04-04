bb4e8386e93129908571ca3ee5342b32f8453cef
Jeff Loiselle <jeff@loiselles.com>
2017-04-04 12:01:18 -0500

    fixed tag editing

--- admin/src/app/main/tags_edit.controller.js
+++ admin/src/app/main/tags_edit.controller.js
@@ -12,13 +12,17 @@
     vm.tag = tag;
     vm.tags = tags;
 
-    vm.tag.parent_id = _.find(vm.tags, { id: +$state.params.parent_id }).id;
-    vm.parent = _.find(vm.tags, { id: +$state.params.parent_id });
+    if ($state.params.parent_id) {
+      vm.tag.parent_id = _.find(vm.tags, { id: +$state.params.parent_id }).id;
+      vm.parent = _.find(vm.tags, { id: +$state.params.parent_id });
+    } else {
+      vm.parent = _.find(vm.tags, { id: tag.parent_id });
+    }
 
     vm.remove = function() {
       if (confirm('Are you sure you want to delete?')) {
         vm.tag.remove().then(function(){
-          $state.go('admin.tags');
+          $state.go('admin.tags', { id: vm.parent.id });
         });
       }
     }
@@ -26,13 +30,11 @@
     vm.save = function() {
       if (!('id' in vm.tag)) {
         Restangular.all('tags').post(vm.tag).then(function(res){
-          window.history.back();
-          // $state.go('admin.tags');
+          $state.go('admin.tags', { id: vm.parent.id });
         });
       } else {
         vm.tag.put().then(function(res){
-          window.history.back();
-          // $state.go('admin.tags');
+          $state.go('admin.tags', { id: vm.parent.id });
         });
       }
     }
