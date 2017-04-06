86f34eb5d5386a1fcfeda9cb7246fc982af7db34
Jack Turner <jack@turner.io>
2017-04-06 19:51:47 -0400

    Additional interactivity and styling of explorer

--- ui/js/explorer.js
+++ ui/js/explorer.js
@@ -2,8 +2,14 @@ $( function() {
 
   var base = window.location.protocol + '//' + window.location.hostname + '/api/'
 
-  $( '.filter' ).on( 'click', '.filter-toggle', function() {
-    $( this ).parent().toggleClass( 'show-expanded' )
+  $( '.filter' ).on( 'click', '.filter-heading', function() {
+
+    var $filter = $( this ).parent( '.filter' ),
+        $expanded = $filter.toggleClass( 'show-expanded' ).find( '.expanded' ),
+        $inner = $expanded.find( '.inner' )
+
+    $expanded.height( $filter.hasClass( 'show-expanded' ) ? $inner.height() : 0 )
+    
   })
 
 } )
