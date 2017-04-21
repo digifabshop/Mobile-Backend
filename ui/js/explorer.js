3ab06bad25481f7bc7cf38d29ca0a00a0d335784
Jack Turner <jack@turner.io>
2017-04-20 20:02:28 -0400

    Fixing bug in previous commit

--- ui/js/explorer.js
+++ ui/js/explorer.js
@@ -2,7 +2,7 @@ $( function() {
 
   var BASE = window.location.protocol + '//' + window.location.hostname + '/api/',
       TAGS = [],
-      $material_categories = $( '#material-categories' ),
+      $materials_categories = $( '#materials-categories' ),
       $content_categories = $( '#content-categories' )
 
   $( '.filter' ).on( 'click', '.filter-heading', function() {
@@ -15,14 +15,16 @@ $( function() {
     
   })
 
-  var build_2_level_tag_filter = function( section_name, $section_el ) {
+  var build_2_level_tag_filter = function( section_name ) {
+
+    var section_name_lower = section_name.toLowerCase(),
+        $section_el = $( '#' + section_name_lower + '-categories' )
 
     // Find the id of the tag with name of the section
     var section_id = _.find( TAGS, { 'name': section_name } ).id
 
     // Collect all the tags that have that id as their parent, these are categories
     var categories = _.filter( TAGS, { 'parent_id': section_id } )
-    console.log( categories )
 
     // For each one of the categories… 
     _.forEach( categories, function( tag ) {
@@ -31,7 +33,7 @@ $( function() {
       var $cat_tag = $( '<div class="tag parent" data-category-id="'+tag.id+'">'+tag.name+'<span class="triangle"></span></div>' )
 
       // Attach them to the DOM
-      $material_categories.append( $cat_tag )
+      $section_el.append( $cat_tag )
 
       // Create a DOM element to contain the items in that category
       var $children_wrapper = $( '<div class="children hide" data-category-id="'+tag.id+'" />')
@@ -48,30 +50,30 @@ $( function() {
       } )
 
       // Attach the container to the DOM
-      $material_categories.after( $children_wrapper )
+      $section_el.after( $children_wrapper )
       
       // Attach click events to material categories to toggle visibilty of related container of children
       $cat_tag.on( 'click', function() {
         
         var $this = $( this ),
             clicked_category_id = $this.data( 'category-id' ),
-            $this_wrapper = $( '#materials-filter .children[data-category-id='+clicked_category_id+']' )
+            $this_wrapper = $( '#'+section_name_lower+'-filter .children[data-category-id='+clicked_category_id+']' )
 
-        $( '#materials-filter .tag.parent' ).not( $this ).hide()
+        $( '#'+section_name_lower+'-filter .tag.parent' ).not( $this ).hide()
 
         if( $this.hasClass( 'active' ) ) {
-          $( '#materials-filter .tag.parent' ).show()
+          $( '#'+section_name_lower+'-filter .tag.parent' ).show()
           $this.removeClass( 'active' )
           $this_wrapper.addClass( 'hide' )
         } else {
-          $( '#materials-filter .tag.parent.active').removeClass( 'active' )
+          $( '#'+section_name_lower+'-filter .tag.parent.active').removeClass( 'active' )
           $this.addClass( 'active' )
-          $( '#materials-filter .children').addClass( 'hide' )
+          $( '#'+section_name_lower+'-filter .children').addClass( 'hide' )
           $this_wrapper.removeClass( 'hide' )
-          $( '#materials-filter .tag.parent' ).not( $this ).hide()
+          $( '#'+section_name_lower+'-filter .tag.parent' ).not( $this ).hide()
         }
 
-        $( '#materials-filter .expanded').height( $( '#materials-filter .expanded .inner').height() )
+        $( '#'+section_name_lower+'-filter .expanded').height( $( '#'+section_name_lower+'-filter .expanded .inner').height() )
 
       } )
 
@@ -83,8 +85,8 @@ $( function() {
 
     TAGS = r.data
 
-    build_2_level_tag_filter( 'Materials', $material_categories )
-    build_2_level_tag_filter( 'Content', $content_categories )
+    build_2_level_tag_filter( 'Materials' )
+    build_2_level_tag_filter( 'Content' )
 
   } )
 
